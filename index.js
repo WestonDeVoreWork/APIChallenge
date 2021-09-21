ee46bf152dc88a409d6148d203b92155f7e15b8b
Weston DeVore <westondevorebusiness@gmail.com>
2021-09-21 18:22:03 -0400

    Some basic css and html page setup.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // API CHALLENGE DUE ON THE 25th
+let baseURL = 'https://cat-fact.herokuapp.com/facts'
+let key = 
+fetch('https://alexwohlbruck.github.io/cat-facts/')
+    .then(function(res) {
+
+    })
