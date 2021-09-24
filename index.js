a751a9f2ccfc94c42b704500fbf63edd66efc81c
Weston DeVore <westondevorebusiness@gmail.com>
2021-09-23 22:41:26 -0400

    fixed fetch logic and changed api's, worked on styling

--- index.js
+++ index.js
@@ -1,7 +1,51 @@
 // API CHALLENGE DUE ON THE 25th
-let baseURL = 'https://cat-fact.herokuapp.com/facts'
-let key = 
-fetch('https://alexwohlbruck.github.io/cat-facts/')
-    .then(function(res) {
 
-    })
+/* 
+documentaion below:
+https://catfact.ninja/
+*/
+
+let baseURL = 'https://catfact.ninja/fact'
+const button = document.getElementById("button")
+
+
+button.addEventListener('click', fastFact)
+
+    async function fastFact(e) {
+        e.preventDefault();
+
+        // let getfastFact = (baseURL, value) => fetch(baseURL, {mode: "cors"}) //fetch
+        const response = await fetch(baseURL)
+        const json = await response.json();
+        // console.log(json);
+        giveFact(json);  
+        
+
+        // console.log('3');
+        // console.log(randomFact);
+            // .then
+            // .then(json => console.log(json))
+            // .catch;
+
+            // console.log(GET /facts/random);
+                // const response = await fetch(baseURL)
+                    // console.log(response);
+                    // const json = await response.json();
+                    // console.log(json);
+       
+       
+                    // console.log(randomFact);
+    };
+
+let giveFact = data => {
+            // console.log(data);
+            //const randomFact = data;
+            const randomFact = JSON.stringify(data.fact);
+            console.log(randomFact)
+            document.getElementById("factDisplay").innerHTML = randomFact;
+            // console.log(randomFact);
+            // console.log('2');
+            
+        };
+// fastFact();
+
